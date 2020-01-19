1fc8387a9ad1ba215a68be5b74e66e6eb4c76100
twopheat <twopheat@gmail.com>
2020-01-18 23:16:53 -0800

    definitely a WIP, needing TDD assitance

--- app.js
+++ app.js
@@ -3,5 +3,39 @@ const jest = require("jest");
 
 inquirer
     .prompt([{
-        message: "Pick a manager for your Team:"
-    }])
+        message: "Pick a manager for your Team:",
+        name: "Manager:name"
+    },
+    {
+        message: "Choose at least 1 Engineer:"
+        name: "Engineer:name"
+    },
+    {
+        message: "Choose 1 optional Intern:"
+        name: "Intern:name"
+    }
+    ])
+    .then(function);
+
+
+
+    // getters n methods
+get Name() {
+    return this.name;
+}
+
+get Id() {
+    return this.id;
+}
+
+get Email() {
+    return this.email;
+}
+
+get Role() {
+    return this.title;
+}
+
+get char() {
+    return this.char;
+}
