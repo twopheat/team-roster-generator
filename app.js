987b356275e6f03334cefc37f5e7d20a64622560
twopheat <twopheat@gmail.com>
2020-01-21 10:25:49 -0800

    another push, app runs! now to make builder

--- app.js
+++ app.js
@@ -4,37 +4,37 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 
 function initQ() {
-  inquirer
-    .prompt([{
-        message: "Enter a Name:",
-        name: "name"
-    },
-    {
-        message: "Enter an Email:",
-        name: "email"
-    },
-    {
-        message: "Enter an Id:",
-        name: "id"
-    },
-    {
-        message: "Enter their Role:",
-        name: "role",
-        type: "list",
-        choices: ["Manager", "Engineer", "Intern"]
-    }
-    ])
-    .then(function(res1) {
-        secondQ(res1);
-     //   console.log(res1);
-    });
+    inquirer
+        .prompt([{
+            message: "Enter a Name:",
+            name: "name"
+        },
+        {
+            message: "Enter an Email:",
+            name: "email"
+        },
+        {
+            message: "Enter an Id:",
+            name: "id"
+        },
+        {
+            message: "Enter their Role:",
+            name: "role",
+            type: "list",
+            choices: ["Manager", "Engineer", "Intern"]
+        }
+        ])
+        .then(function (res1) {
+            secondQ(res1);
+            //   console.log(res1);
+        });
 }
 initQ();
 
 function secondQ(res1) {
-  var role = res1.role;
+    var role = res1.role;
     if (role === "Manager") {
-        role = "officeNumber"
+        role = "officenumber"
     }
     else if (role === "Engineer") {
         role = "github"
@@ -43,30 +43,43 @@ function secondQ(res1) {
         role = "school"
     }
     inquirer
-    .prompt([{
-        message: "Enter your " + role,
-        name: "title"
-    }])
-    .then(function(res2) {
-        var data = res1;
-        data ["title"] = res2.title;
-       // thirdQ(data);
-        console.log(data);
-    });
+        .prompt([{
+            message: "Enter your " + role,
+            name: "title"
+        }])
+        .then(function (res2) {
+            var data = res1;
+            data["title"] = res2.title;
+            thirdQ(data);
+            console.log(data);
+        });
 
 }
 //yes or no  - array of constructed objects from Q's - build in Q2, finally empty array (global) to drop info in
-function thirdQ(res) {
-      inquirer
-      .prompt([{
-          message: "Do you want to add Team Member?",
-          name: "title"
-      }])
-      .then(function(res) {
-          console.log(res);
-      });
-  
-  }
+function thirdQ(data) {
+
+    inquirer
+        .prompt([{
+            message: "Do you want to add another team member?",
+            name: "another",
+            type: "list",
+            choices: ["yes", "no"]
+        }])
+        .then(function (another) {
+            var res2 = data;
+            if (another === "yes") {
+                initQ();
+              }
+            else if (another === "no") {
+                buildTemp(res2);
+            }
+            console.log(res2);
+        
+
+        
+    });
+
+}
 
 
 
